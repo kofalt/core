b6d6c3ab498f4bfc486f9e0443d23a20d920ab6a
Justin Ehlert <justinehlert@flywheel.io>
2018-02-02 15:08:53 -0600

    Started working on customized python templates.

--- sdk/codegen/src/main/java/io/flywheel/codegen/PythonGenerator.java
+++ sdk/codegen/src/main/java/io/flywheel/codegen/PythonGenerator.java
@@ -1,12 +1,28 @@
 package io.flywheel.codegen;
 
+import io.swagger.codegen.CodegenConfig;
 import io.swagger.codegen.CodegenModel;
 import io.swagger.codegen.CodegenProperty;
+import io.swagger.codegen.SupportingFile;
 import io.swagger.codegen.languages.PythonClientCodegen;
 import io.swagger.models.ModelImpl;
+import io.swagger.models.Swagger;
 import io.swagger.models.properties.Property;
 
-public class PythonGenerator extends PythonClientCodegen {
+import java.util.HashMap;
+import java.util.Map;
+
+public class PythonGenerator extends PythonClientCodegen implements CodegenConfig {
+
+    private static Map<String, String> propertySubstitutions;
+
+    public PythonGenerator() {
+        super();
+
+        propertySubstitutions = new HashMap<>();
+        propertySubstitutions.put("+", "plus");
+        propertySubstitutions.put("-", "minus");
+    }
 
     @Override
     public String getName() {
@@ -23,4 +39,37 @@ public class PythonGenerator extends PythonClientCodegen {
         }
     }
 
+    @Override
+    public void processOpts() {
+        super.processOpts();
+
+        // Flywheel wrapper file
+        supportingFiles.add(new SupportingFile("flywheel.mustache", packageName, "flywheel.py"));
+    }
+
+    @Override
+    public void preprocessSwagger(Swagger swagger) {
+        FlywheelCodegenSupport.removeExtraOperationTags(swagger);
+    }
+
+    @Override
+    public Map<String, Object> postProcessOperations(Map<String, Object> objs) {
+        objs = super.postProcessOperations(objs);
+        return FlywheelCodegenSupport.postProcessOperations(objs, this);
+    }
+
+    @Override
+    public Map<String, Object> postProcessModels(Map<String, Object> objs) {
+        objs = super.postProcessModels(objs);
+        return FlywheelCodegenSupport.postProcessModels(objs, this);
+    }
+
+    @Override
+    public void postProcessModelProperty(CodegenModel model, CodegenProperty property) {
+        super.postProcessModelProperty(model, property);
+
+        if( propertySubstitutions.containsKey(property.baseName) ) {
+            property.name = property.nameInCamelCase = propertySubstitutions.get(property.baseName);
+        }
+    }
 }
