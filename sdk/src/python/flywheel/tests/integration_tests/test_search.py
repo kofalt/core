1512e02bc671458b1bcb3a26f0377d9e56236266
Justin Ehlert <justinehlert@flywheel.io>
2018-02-09 15:53:25 -0600

    Add integration tests for search and job queue

---
+++ sdk/src/python/flywheel/tests/integration_tests/test_search.py
@@ -0,0 +1,47 @@
+import unittest
+import time
+from sdk_test_case import SdkTestCase
+from test_acquisition import create_test_acquisition
+
+import flywheel
+
+class SearchTestCases(SdkTestCase):
+    def setUp(self):
+        self.group_id, self.project_id, self.session_id, self.acquisition_id = create_test_acquisition()
+
+    def tearDown(self):
+        self.fw.delete_project(self.project_id)
+        self.fw.delete_group(self.group_id)
+
+    # Ref https://github.com/flywheel-io/sdk/issues/31
+    @unittest.skip("No elastic search container")
+    def test_search(self):
+        fw = self.fw
+       
+        acquisition = fw.get_acquisition(self.acquisition_id)
+        self.assertEqual(acquisition.id, self.acquisition_id)
+
+        # Allow time for search to index
+        # Ref https://github.com/flywheel-io/sdk/issues/32
+        time.sleep(1)
+
+        query = flywheel.SearchQuery(
+            return_type='session',
+            search_string=acquisition.label
+        )
+        search_result = fw.search(query)
+        self.assertEqual(len(search_result), 1)
+
+        query = flywheel.SearchQuery(
+            return_type='acquisition',
+            search_string=acquisition.label
+        )
+        search_result = fw.search(query)
+        self.assertEqual(len(search_result), 1)
+        
+
+        
+
+
+
+
