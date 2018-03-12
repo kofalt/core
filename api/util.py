782715d93d75244a4e2633d2f6bed56b74b58d65
Justin Ehlert <justinehlert@flywheel.io>
2018-03-12 10:39:02 -0500

    Rename resolver's node_type to container_type

--- api/util.py
+++ api/util.py
@@ -470,7 +470,7 @@ def parse_pagination_int_param(int_param):
 
     return pagination_int
 
-def add_node_type(request, result):
-    """Adds a 'node_type' property to result if fw_node_type is set in the request environment."""
-    if 'fw_node_type' in request.environ and isinstance(result, dict):
-        result['node_type'] = request.environ['fw_node_type']
+def add_container_type(request, result):
+    """Adds a 'container_type' property to result if fw_container_type is set in the request environment."""
+    if 'fw_container_type' in request.environ and isinstance(result, dict):
+        result['container_type'] = request.environ['fw_container_type']
