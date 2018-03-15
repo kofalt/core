c94c7042b11193f0799eb272cb6a901b0f42857e
Justin Ehlert <justinehlert@flywheel.io>
2018-03-15 15:08:06 -0500

    Add model mixins for tags, permissions, and files

--- sdk/src/python/tests/integration_tests/test_mixins.py
+++ sdk/src/python/tests/integration_tests/test_mixins.py
@@ -43,13 +43,13 @@ class MixinTestCases(SdkTestCase):
         self.assertIsNotNone(children)
 
         self.assertEqual(len(children), 2)
-        r_file = children[0]
-        self.assertEqual(r_file.name, 'yeats.txt')
-        self.assertEqual(r_file.size, len(poem))
-
-        r_session = children[1]
+        r_session = children[0]
         self.assertEqual(r_session.id, self.session_id)
         self.assertEqual(r_session.project, self.project_id)
+
+        r_file = children[1]
+        self.assertEqual(r_file.name, 'yeats.txt')
+        self.assertEqual(r_file.size, len(poem))
 
         acquisitions = r_session.get_acquisitions()
         self.assertIsNotNone(acquisitions)
@@ -58,6 +58,37 @@ class MixinTestCases(SdkTestCase):
         r_acquisition = acquisitions[0]
         self.assertEqual(r_acquisition.id, self.acquisition_id)
         self.assertEqual(r_acquisition.session, self.session_id)
+
+    def test_tag_mixins(self):
+        fw = self.fw
+
+        for container_type in ['group', 'project', 'session', 'acquisition']:
+            cid = getattr(self, '{0}_id'.format(container_type), None)
+            self.assertIsNotNone(cid)
+
+            getter = getattr(self.fw, 'get_{0}'.format(container_type), None)
+            self.assertIsNotNone(getter)
+
+            container = getter(cid)
+            self.assertIsNotNone(container)
+
+            initial_tags = container.tags
+            self.assertFalse(bool(initial_tags))
+
+            container.add_tag('tag1')
+            container.add_tag('tag2')
+
+            container = getter(cid)
+            self.assertEqual(2, len(container.tags))
+            self.assertIn('tag1', container.tags)
+            self.assertIn('tag2', container.tags)
+
+            # Rename and delete
+            container.rename_tag('tag2', 'tag3')
+            container.delete_tag('tag1')
+
+            container = getter(cid)
+            self.assertEqual(['tag3'], container.tags)
 
 
 
