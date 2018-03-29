8e7d06cb518570ac0366f85f686c2b06f14fc4cf
Justin Ehlert <justinehlert@flywheel.io>
2018-03-29 13:21:24 -0500

    Move core build steps back into .travis.yml

---
+++ bin/build-sdk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ev
+
+if [ "$#" -ne 2 ]; then
+	print "Usage: build-sdk.sh <sdk version> <doc version>"
+	exit 1
+fi
+
+SDK_VERSION=$1
+DOC_VERSION=$2
+
+# Build Swagger
+swagger/make.sh $DOC_VERSION
+
+# Copy swagger.json into place
+cp swagger/build/swagger-codegen.json sdk/swagger.json
+
+# Build SDK
+sdk/make.sh $SDK_VERSION
+
+# Test SDK (Python 3.4)
+sdk/scripts/docker-tests.sh --image core:testing 
+
+# Test SDK (Python 2.7)
+sdk/scripts/docker-tests.sh --image core:testing --python2
+
+# Copy artifacts
+mkdir -p dist/sdk/
+cp sdk/dist/* dist/sdk/
+
