e474a25698721c7c11441408e46892eebb910713
Ambrus Simon <ambrussimon@invenshure.com>
2018-04-03 16:36:04 +0200

    Fix testing issues; * Run uwsgi within tests.sh (fly/fly and generic storage expectation); * Consequently move coverage collection and small cmds to tests.sh; * Re-enable (fix) selecting a single integration test via "pytest -k"; * Improve test output (eg. print exit code last);

--- tests/bin/tests.sh
+++ tests/bin/tests.sh
@@ -19,11 +19,8 @@ Options:
     -- PYTEST_ARGS       Arguments passed to py.test
 
 Envvars (required for integration tests):
-    SCITRAN_SITE_API_URL            URI to a running core instance (including /api)
-    SCITRAN_CORE_DRONE_SECRET       API shared secret
     SCITRAN_PERSISTENT_DB_URI       Mongo URI to the scitran DB
     SCITRAN_PERSISTENT_DB_LOG_URI   Mongo URI to the scitran log DB
-
 "
 
 
@@ -51,32 +48,59 @@ main() {
         shift
     done
 
+    log "INFO: Cleaning pyc and previous coverage results ..."
+    find . -type d -name __pycache__ -exec rm -rf {} \;
+    find . -type f -name '*.pyc' -delete
+    rm -rf .coverage htmlcov
+
+    log "INFO: Staring core ..."
+    export SCITRAN_CORE_DRONE_SECRET=${SCITRAN_CORE_DRONE_SECRET:-change-me}
+    uwsgi --ini /var/scitran/config/uwsgi-config.ini --http [::]:9000 \
+        --env SCITRAN_COLLECT_ENDPOINTS=true \
+        --env SCITRAN_CORE_ACCESS_LOG_ENABLED=true \
+        --env SCITRAN_CORE_LOG_LEVEL=debug \
+        --env SCITRAN_RUNTIME_COVERAGE=true \
+        --processes 1 --threads 1 --enable-threads \
+        --http-keepalive --so-keepalive --add-header "Connection: Keep-Alive" \
+        --logformat '[%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) request_id=%(request_id)' \
+        >/tmp/core.log 2>&1 &
+    export CORE_PID=$!
+    export SCITRAN_SITE_API_URL=http://localhost:9000/api
+
     log "INFO: Running unit tests ..."
-    py.test --exitfirst --cov=api --cov-report= tests/unit_tests/python $PYTEST_ARGS
+    # pytest exits with 5 when no tests were selected at all: do not exit prematurely when
+    # selecting integration tests via `pytest -k` (and thereby deselecting all unit tests)
+    py.test --exitfirst --cov=api --cov-report= tests/unit_tests/python $PYTEST_ARGS || [ $? = 5 ]
 
     log "INFO: Running integration tests ..."
-    if [ "${0##*/}" = "run-tests-ubuntu.sh" ]; then
-        # Temporary fly/fly backwards compatibility workaround
-        # TODO (Ambrus) Remove together with sh symlink
-        export SCITRAN_SITE_API_URL=http://localhost:9000/api
-        uwsgi --ini /var/scitran/config/uwsgi-config.ini --http [::]:9000 \
-            --processes 1 --threads 1 --enable-threads \
-            --http-keepalive --so-keepalive --add-header "Connection: Keep-Alive" \
-            --logformat '[%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) request_id=%(request_id)' \
-        &
-    fi
-
-    touch tests/running_integration
-    py.test --exitfirst tests/integration_tests/python $PYTEST_ARGS
-    rm tests/running_integration
+    py.test --exitfirst tests/integration_tests/python $PYTEST_ARGS || tail_logs_and_exit $?
 
     log "INFO: Running pylint ..."
     # TODO Enable Refactor and Convention reports
     # TODO Move --disable into rc
     pylint --rcfile=tests/.pylintrc --jobs=4 --reports=no --disable=C,R,W0312,W0141,W0110 api
 
-    # log "Running pep8 ..."
+    # log "INFO: Running pep8 ..."
     # pep8 --max-line-length=150 --ignore=E402 api
+
+    log "INFO: Stopping core ..."
+    kill $CORE_PID || true
+    wait 2>/dev/null
+
+    log "INFO: Collecting coverage ..."
+    coverage combine
+    coverage html
+    coverage xml
+    coverage report --skip-covered --show-missing
+}
+
+
+tail_logs_and_exit() {
+    log "INFO: Tailing core logs ..."
+    tail --lines=10 /tmp/core.log
+    rm -f tests/running_integration
+
+    exit $1
 }
 
 
