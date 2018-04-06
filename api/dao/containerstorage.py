93a0ec4fef989d37ff5180d0faf3af62a1e861a0
Justin Ehlert <justinehlert@flywheel.io>
2018-04-06 13:34:16 -0500

    Fixup after rebase

--- api/dao/containerstorage.py
+++ api/dao/containerstorage.py
@@ -295,17 +295,17 @@ class SessionStorage(ContainerStorage):
         return SubjectStorage().get_container(cont['subject']['_id'], projection=projection)
 
 
-    def get_all_el(self, query, user, projection, fill_defaults=False, pagination=None):
+    def get_all_el(self, query, user, projection, fill_defaults=False, pagination=None, **kwargs):
         """
         Override allows 'collections' key in the query, will transform into proper query for the caller and return results
         """
         if query and query.get('collections'):
             # Find acquisition ids in this collection, add to query
             collection_id = query.pop('collections')
-            a_ids = AcquisitionStorage().get_all_el({'collections': bson.ObjectId(collection_id)}, None, {'session': 1})
+            a_ids = AcquisitionStorage().get_all_el({'collections': bson.ObjectId(collection_id)}, None, {'session': 1}, **kwargs)
             query['_id'] = {'$in': list(set([a['session'] for a in a_ids]))}
 
-        return super(SessionStorage, self).get_all_el(query, user, projection, fill_defaults=fill_defaults, pagination=pagination)
+        return super(SessionStorage, self).get_all_el(query, user, projection, fill_defaults=fill_defaults, pagination=pagination, **kwargs)
 
 
     def recalc_session_compliance(self, session_id, session=None, template=None, hard=False):
