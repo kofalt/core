fb40994feb0945cbb40c9cc6dadcf45c985b9646
Justin Ehlert <justinehlert@flywheel.io>
2018-04-12 11:23:53 -0500

    Increase testing for analyses and fix found issues

--- sdk/src/python/tests/integration_tests/test_analysis.py
+++ sdk/src/python/tests/integration_tests/test_analysis.py
@@ -15,7 +15,108 @@ class AnalysisTestCases(SdkTestCase):
         self.fw.delete_group(self.group_id)
         self.fw.delete_gear(self.gear_id)
 
-    def test_analysis(self):
+    def test_ad_hoc_analysis(self):
+        fw = self.fw
+        
+        # Upload to session
+        poem = 'A gaze blank and pitiless as the sun,'
+        fw.upload_file_to_session(self.session_id, flywheel.FileSpec('yeats.txt', poem))
+
+        file_ref = flywheel.FileReference(
+            id=self.session_id,
+            type='session',
+            name='yeats.txt'
+        )
+
+        analysis = flywheel.AnalysisInput(label=self.rand_string(), description=self.rand_string(), inputs=[file_ref])
+
+        # Add
+        analysis_id = fw.add_session_analysis(self.session_id, analysis)
+        self.assertNotEmpty(analysis_id)
+
+        session = fw.get_session(self.session_id)
+        self.assertEqual(len(session.analyses), 1)
+
+        r_analysis = session.analyses[0]
+        self.assertEqual(r_analysis.id, analysis_id)
+        self.assertIsNone(r_analysis.job)
+        self.assertTimestampBeforeNow(r_analysis.created)
+        self.assertGreaterEqual(r_analysis.modified, r_analysis.created)
+        self.assertEqual(len(r_analysis.inputs), 1)
+        self.assertEqual(r_analysis.inputs[0].name, 'yeats.txt')
+
+        # Access analysis directly
+        r_analysis2 = fw.get_analysis(analysis_id)
+        self.assertEqual(r_analysis, r_analysis2)
+
+        # Analysis Notes
+        text = self.rand_string()
+        fw.add_session_analysis_note(self.session_id, analysis_id, text)
+
+        # Check
+        session = fw.get_session(self.session_id)
+        self.assertEqual(len(session.analyses), 1)
+
+        r_analysis = session.analyses[0]
+        self.assertEqual(len(r_analysis.notes), 1)
+        self.assertNotEmpty(r_analysis.notes[0].user)
+        self.assertEqual(r_analysis.notes[0].text, text)
+        self.assertTimestampBeforeNow(r_analysis.notes[0].created)
+        self.assertTimestampBeforeNow(r_analysis.notes[0].modified)
+        self.assertTimestampAfter(r_analysis.notes[0].modified, r_analysis.created)
+        self.assertGreaterEqual(r_analysis.notes[0].modified, r_analysis.notes[0].created)
+
+        # Access multiple analyses
+        analysis_id2 = fw.add_session_analysis(self.session_id, analysis)
+        self.assertNotEmpty(analysis_id2)
+
+        # Try getting analysis incorrectly
+        self.assertRaises(flywheel.ApiException, fw.get_analyses, 'session', self.session_id, 'projects')
+
+        # Get all Session level analyses in group
+        analyses = fw.get_analyses('groups', self.group_id, 'sessions')
+        self.assertEqual(len(analyses), 2)
+
+        self.assertEqual(1, len(list(filter(lambda x: x.id == r_analysis.id, analyses))))
+        self.assertEqual(1, len(list(filter(lambda x: x.id == analysis_id2, analyses))))
+
+        # Get project level analyses in group (will be zero)
+        analyses = fw.get_analyses('groups', self.group_id, 'projects')
+        self.assertEmpty(analyses)
+
+        # Info, tags
+        tag = 'example-tag'
+        fw.add_analysis_tag(analysis_id, tag)
+
+        # Replace info
+        fw.replace_analysis_info(analysis_id, {
+            'foo': 3,
+            'bar': 'qaz'
+        })
+
+        # Set info
+        fw.set_analysis_info(analysis_id, {
+            'foo': 42,
+            'hello': 'world'
+        })
+
+        # Check
+        r_analysis = fw.get_analysis(analysis_id)
+        self.assertEqual(r_analysis.tags, [tag])
+        self.assertEqual(r_analysis.info, {
+            'foo': 42,
+            'bar': 'qaz',
+            'hello': 'world'
+        })
+
+        # Delete info fields
+        fw.delete_analysis_info_fields(analysis_id, ['foo', 'bar'])
+        r_analysis = fw.get_analysis(analysis_id)
+        self.assertEqual(r_analysis.info, {
+            'hello': 'world'
+        })
+
+    def test_job_based_analysis(self):
         fw = self.fw
         
         # Upload to session
@@ -55,52 +156,47 @@ class AnalysisTestCases(SdkTestCase):
         self.assertEqual(len(r_analysis.inputs), 1)
         self.assertEqual(r_analysis.inputs[0].name, 'yeats.txt')
 
+        # Verify job
+        r_job = fw.get_job(r_analysis.job.id)
+        self.assertEqual(r_analysis.job.state, 'pending')
+
         # Access analysis directly
         r_analysis2 = fw.get_analysis(analysis_id)
         self.assertEqual(r_analysis, r_analysis2)
 
-        # "Run the job"
-        fw.change_job_state(r_analysis.job.id, 'running')
+    def test_analysis_files(self):
+        fw = self.fw
+        
+        # Upload to session
+        poem = 'A gaze blank and pitiless as the sun,'
+        fw.upload_file_to_session(self.session_id, flywheel.FileSpec('yeats.txt', poem))
 
-        #
-        ## We can't test further than this, because /engine requires drone.
-        ## See analysis_test.go in flywheel-io/sdk
-        #
-        fw.change_job_state(r_analysis.job.id, 'complete')
+        file_ref = flywheel.FileReference(
+            id=self.session_id,
+            type='session',
+            name='yeats.txt'
+        )
 
-        # Analysis Notes
-        text = self.rand_string()
-        fw.add_session_analysis_note(self.session_id, analysis_id, text)
+        analysis = flywheel.AnalysisInput(label=self.rand_string(), description=self.rand_string(), inputs=[file_ref])
 
-        # Check
-        session = fw.get_session(self.session_id)
-        self.assertEqual(len(session.analyses), 1)
+        # Add
+        analysis_id = fw.add_session_analysis(self.session_id, analysis)
 
-        r_analysis = session.analyses[0]
-        self.assertEqual(len(r_analysis.notes), 1)
-        self.assertNotEmpty(r_analysis.notes[0].user)
-        self.assertEqual(r_analysis.notes[0].text, text)
-        self.assertTimestampBeforeNow(r_analysis.notes[0].created)
-        self.assertTimestampBeforeNow(r_analysis.notes[0].modified)
-        self.assertTimestampAfter(r_analysis.notes[0].modified, r_analysis.created)
-        self.assertGreaterEqual(r_analysis.notes[0].modified, r_analysis.notes[0].created)
+        # Download the input file and check content
+        self.assertDownloadFileTextEquals(fw.download_input_from_analysis_as_data, analysis_id, 'yeats.txt', poem)
+        self.assertDownloadFileTextEqualsWithTicket(fw.get_analysis_input_download_url, analysis_id, 'yeats.txt', poem)
 
-        # Access multiple analyses
-        analysis_id2 = fw.add_session_analysis(self.session_id, analysis)
-        self.assertNotEmpty(analysis_id2)
+        poem_out = 'Surely the Second Coming is at hand.'
+        fw.upload_output_to_analysis(analysis_id, flywheel.FileSpec('yeats-out.txt', poem_out))
 
-        # Try getting analysis incorrectly
-        self.assertRaises(flywheel.ApiException, fw.get_analyses, 'session', self.session_id, 'projects')
+        r_analysis = fw.get_analysis(analysis_id)
+        self.assertEqual(len(r_analysis.files), 1)
+        self.assertEqual(r_analysis.files[0].name, 'yeats-out.txt')
+        self.assertEqual(r_analysis.files[0].size, 36)
+        self.assertEqual(r_analysis.files[0].mimetype, 'text/plain')
 
-        # Get all Session level analyses in group
-        analyses = fw.get_analyses('groups', self.group_id, 'sessions')
-        self.assertEqual(len(analyses), 2)
-
-        self.assertEqual(1, len(list(filter(lambda x: x.id == r_analysis.id, analyses))))
-        self.assertEqual(1, len(list(filter(lambda x: x.id == analysis_id2, analyses))))
-
-        # Get project level analyses in group (will be zero)
-        analyses = fw.get_analyses('groups', self.group_id, 'projects')
-        self.assertEmpty(analyses)
+        # Download and check content
+        self.assertDownloadFileTextEquals(fw.download_output_from_analysis_as_data, analysis_id, 'yeats-out.txt', poem_out)
+        self.assertDownloadFileTextEqualsWithTicket(fw.get_analysis_output_download_url, analysis_id, 'yeats-out.txt', poem_out)
 
 
