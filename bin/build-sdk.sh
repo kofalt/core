36dd449abb9d1cdf04dda2e5b52a4abfaf9757da
Szilveszter Balogh <szilveszterbalogh@invenshure.com>
2018-05-04 12:05:33 +0200

    Transform build sdk to only copy the dist files

--- bin/build-sdk.sh
+++ bin/build-sdk.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 set -ev
 
-if [ "$#" -ne 2 ]; then
-	print "Usage: build-sdk.sh <sdk version> <doc version>"
+if [ "$#" -ne 0 ]; then
+	print "Usage: build-sdk.sh"
 	exit 1
 fi
 
-SDK_VERSION=$1
-DOC_VERSION=$2
-
-# Build Swagger
-swagger/make.sh $DOC_VERSION
-
-# Build SDK
-sdk/make.sh $SDK_VERSION
-
-# Test SDK (Python 3.4)
-sdk/scripts/docker-tests.sh --image core:testing 
-
-# Test SDK (Python 2.7)
-sdk/scripts/docker-tests.sh --image core:testing --python2
-
 # Copy artifacts
 mkdir -p dist/
-cp sdk/dist/* dist/
+docker run --name temp_name core:sdk_build /bin/true
+docker cp temp_name:/local/src/python/gen/dist/. dist
+docker rm temp_name
 
+docker run --name temp_name core:gradle /bin/true
+docker cp temp_name:/local/src/matlab/build/distributions/. dist
+docker rm temp_name
