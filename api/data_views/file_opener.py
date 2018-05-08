9d55eb932b0f313222d1873d65737e663eaf5297
Justin Ehlert <justinehlert@flywheel.io>
2018-05-08 14:46:38 -0500

    Complete pipeline rework to support files

--- api/data_views/file_opener.py
+++ api/data_views/file_opener.py
@@ -1,8 +1,10 @@
 import os
-import zipfile
 import gzip
+import time
+import zipfile
 
 from .. import files
+from .util import filtered_container_list
 
 # TODO: Should we support tarfiles?
 class FileOpener(object):
@@ -10,15 +12,17 @@ class FileOpener(object):
 
     FileOpener will close any open files when the context is exited.
     """
-    def __init__(self, file_entry, zip_filter):
+    def __init__(self, file_entry, zip_filter, match_type='first'):
         """Create a new FileOpener
 
         Arguments:
             file_entry (dict): The file entry as retrieved from the database
             zip_filter (regex): The regular expression to match zip entries, if applicable
+            match_type (str): The optional match type if multiple zip entries are matched (default is first)
         """
         self.file_entry = file_entry
         self.zip_filter = zip_filter
+        self.match_type = match_type
 
         # This is the opened file_entry file
         self._system_fd = None
@@ -26,21 +30,14 @@ class FileOpener(object):
         # This is the optional ZipFile instance
         self._zipfile = None
 
+        # This is the optional set of zipfile entries
+        self._zip_entries = []
+
         # This is the final _fd for reading
         self._fd = None
 
         # The final filename
         self._name = file_entry['name']
-
-    @property
-    def name(self):
-        """str: The name of the opened file"""
-        return self._name.lower()
-
-    @property
-    def fd(self):
-        """file: The currently opened file"""
-        return self._fd
 
     def is_gzip(self):
         """Check if the file described by file_entry is a gzip file"""
@@ -64,17 +61,19 @@ class FileOpener(object):
                 self._zipfile = zipfile.ZipFile(self._system_fd)
 
                 # Find zip entry
-                matched_file = None
-                for path in self._zipfile.namelist():
-                    if self.zip_filter.match(path):
-                        matched_file = path
-                        break
-                        
-                if not matched_file:
+                zip_entries = []
+                for zipinf in self._zipfile.infolist():
+                    zip_entries.append({
+                        'path': zipinf.filename,
+                        'timestamp': time.mktime(zipinf.date_time + (0, 0, -1))
+                    })
+
+                self._zip_entries = filtered_container_list(zip_entries, 'path', self.zip_filter, 
+                        match_type=self.match_type, date_key='timestamp') 
+
+                if not self._zip_entries:
                     raise RuntimeError('Could not find matching zip entry in zip file: {}'.format(self.file_entry['name']))
 
-                self._fd = self._zipfile.open(matched_file, 'r')
-                self._name = matched_file 
             elif gz:
                 # Open as gzip
                 self._fd = gzip.GzipFile(fileobj=self._system_fd, mode='r')
@@ -91,6 +90,15 @@ class FileOpener(object):
     def __exit__(self, exception_type, exception_value, traceback):
         self.close()
 
+    def files(self):
+        """Iterator for each of the files opened by this file opener"""
+        if self._fd:
+            yield self._name, self._fd
+        elif self._zipfile:
+            for entry in self._zip_entries:
+                fd = self._zipfile.open(entry['path'], 'r')
+                yield entry['path'], fd
+
     def close(self):
         """Close any open files"""
         # NOTE: This function should be re-entrant
@@ -101,4 +109,5 @@ class FileOpener(object):
         if self._system_fd:
             self._system_fd.close()
             self._system_fd = None
+
 
