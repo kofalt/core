1b6c82b39d2be177cc69609461871c6b5a4e4a51
Justin Ehlert <justinehlert@flywheel.io>
2018-05-08 15:21:02 -0500

    Add support for analyses with multiple files

--- api/data_views/pipeline/match_containers.py
+++ api/data_views/pipeline/match_containers.py
@@ -1,6 +1,20 @@
 from .pipeline import PipelineStage
 from ..util import filtered_container_list, file_filter_to_regex
 
+def pop_collection(cont, key):
+    result = []
+    parts = key.split('.')
+
+    for part in parts[:-1]:
+        cont = cont.get(part)
+        if not cont:
+            break
+
+    if cont:
+        result = cont.pop(parts[-1], [])
+
+    return result
+
 class MatchContainers(PipelineStage):
     def __init__(self, collection_key, name_key, output_key, filter_spec, match_type):
         super(MatchContainers, self).__init__()
@@ -11,15 +25,6 @@ class MatchContainers(PipelineStage):
         self.filter_regex = file_filter_to_regex(filter_spec)
         self.match_type = match_type
 
-    def match_files(self, files):
-        # Find a matching, not-deleted file
-        if files is not None:
-            for f in files:
-                if 'deleted' in f:
-                    continue
-                if self.file_filter.match(f['name']):
-                    yield f
-
     def process(self, payload):
         # Keep unmatched values at the end
         rows = []
@@ -27,9 +32,9 @@ class MatchContainers(PipelineStage):
 
         # Pop and filter the containers from payload
         for row in payload:
-            containers = row.pop(self.collection_key, [])
+            containers = pop_collection(row, self.collection_key)
             containers = filtered_container_list(containers, self.name_key, self.filter_regex, self.match_type)
-            
+
             if containers:
                 # Emit one row per match
                 for entry in containers:
