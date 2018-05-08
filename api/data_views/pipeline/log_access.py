b3edc77f9cd4c27ffc861a203b0948a808bd3c2d
Justin Ehlert <justinehlert@flywheel.io>
2018-05-08 11:09:30 -0500

    Phase 1 of refactor to pipeline pattern

---
+++ api/data_views/pipeline/log_access.py
@@ -0,0 +1,54 @@
+from .pipeline import PipelineStage, EndOfPayload
+
+from ..access_logger import create_access_logger, is_phi_field
+
+class LogAccess(PipelineStage):
+    def __init__(self, config):
+        super(LogAccess, self).__init__()
+        self.config = config
+        self.logger = create_access_logger()
+        self.request = None
+        self.origin = None
+
+    def initialize(self, tree):
+        for container, field, _ in self.config.columns:
+            # Any subject fields and any info fields are considered PHI
+            if is_phi_field(container, field):
+                next_part = field.split('.')[0]
+                # TODO: Remove check when subject is formalized
+                if next_part == 'subject':
+                    self.logger.add_container('subject')
+                else:
+                    self.logger.add_container(container)
+        
+        if self.config.file_container:
+            self.logger.set_file_container(self.config.file_container)
+
+        self.logger.create_context(tree)
+
+    def set_request_origin(self, request, origin):
+        self.request = request
+        self.origin = origin
+
+    def process(self, rows):
+        for row in rows:
+            meta = row.pop('_meta')
+
+            file_entry = row.get('file', None)
+            if file_entry:
+                filename = file_entry['name']
+            else:
+                filename = None
+
+            self.logger.add_entries(meta, filename)
+
+        # Write all of the access logs. If this fails, abort the request (allow exception to pass through)
+        self.logger.write_logs(self.request, self.origin)
+
+        # Now pass along each row, one at a time
+        for row in rows:
+            self.emit(row)
+
+        self.emit(EndOfPayload)
+
+
