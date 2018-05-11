9bb05e4aaf541d36d8254f634fec663b9ebcf263
David Farkas <david.farkas93@gmail.com>
2018-05-11 15:48:45 +0200

    Fix required field check

--- api/upload.py
+++ api/upload.py
@@ -204,7 +204,7 @@ class Upload(base.RequestHandler):
         metadata = payload.get('metadata', None)
         filenames = payload.get('filenames', None)
 
-        if not (metadata and filenames):
+        if metadata is None or not filenames:
             self.abort(400, 'metadata and at least one filename are required')
 
         tempdir = str(uuid.uuid4())
