7ca122390e320fc9612a4beedc3afaa38d2779c6
Ambrus Simon <ambrussimon@invenshure.com>
2018-06-04 15:59:31 +0200

    Unify teamplay config retrieval, fix isOperable status code on error

--- api/handlers/teamplayhandler.py
+++ api/handlers/teamplayhandler.py
@@ -12,17 +12,18 @@ from ..web import base, errors
 
 class TeamplayHandler(base.RequestHandler):
 
-    @property
-    def config(self):
-        return config.get_auth('teamplay')
-
     def echo(self):
         """Return `echo` query param's value in plaintext. (see teamplay webhook spec)"""
         self.response.write(self.request.GET.get('echo'))
 
     def event(self):
         """Create event doc in mongo from payload as-is. (see teamplay webhook spec)"""
-        secret = bytes(self.config['webhook_secret'])
+        try:
+            teamplay_config = config.get_auth('teamplay')
+            secret = bytes(teamplay_config['webhook_secret'])
+        except KeyError:
+            self.abort(503, 'Teamplay DICOM Webhook not configured')
+
         expected_hash = self.request.headers.get('ms-signature', '').replace('sha256=', '')
         computed_hash = hmac.new(secret, msg=self.request.body, digestmod=hashlib.sha256).hexdigest()
         if computed_hash != expected_hash:
@@ -35,14 +36,19 @@ class TeamplayHandler(base.RequestHandler):
     @require_drone
     def get_token(self):
         """Get and return app JWT to be used by reaper. (see teamplay auth spec)"""
-        response = requests.post(self.config['token_endpoint'],
+        try:
+            teamplay_config = config.get_auth('teamplay')
+        except KeyError:
+            self.abort(503, 'Teamplay SSO not configured')
+
+        response = requests.post(teamplay_config['token_endpoint'],
             headers={
-                'Ocp-Apim-Subscription-Key': self.config['api_key']
+                'Ocp-Apim-Subscription-Key': teamplay_config['client_secret'],
             },
             data={
                 'grant_type': 'client_credentials',
-                'client_id': self.config['client_id'],
-                'client_secret': self.config['client_secret'],
+                'client_id': teamplay_config['client_id'],
+                'client_secret': teamplay_config['client_secret'],
             }
         )
         token = response.json()
@@ -50,12 +56,12 @@ class TeamplayHandler(base.RequestHandler):
 
     @require_drone
     def get_queue(self):
-        """"""
+        """Return teamplay event reap queue"""
         return config.db.teamplay.find({'deleted': {'$exists': False}})
 
     @require_drone
     def reap_item(self, _id):
-        """"""
+        """Remove teamplay event from reap queue"""
         result = config.db.teamplay.update_one({'_id': bson.ObjectId(_id)}, {'$set': {'deleted': datetime.datetime.utcnow()}})
         if result.modified_count != 1:
             raise errors.APINotFoundException('Could not find queue item ' + _id)
@@ -81,21 +87,17 @@ class TeamplayHandler(base.RequestHandler):
           - Others?
         """
         err = []
+        cfg = config.get_config()
 
-        try:
-            config.get_auth('teamplay')
-        except KeyError:
+        if 'teamplay' not in cfg['auth']:
             err.append('Teamplay SSO not configurated.')
-
-        try:
-            config.get_item('teamplay', 'webhook_secret')
-        except KeyError:
+        elif 'webhook_secret' not in cfg['auth']['teamplay']:
             err.append('Teamplay DICOM Webhook not configurated.')
 
         # Test reaper here
 
         if err:
+            self.response.status = 503
             return {'Reason': ' '.join(err)}
-        else:
-            return {}
 
+        return {}
