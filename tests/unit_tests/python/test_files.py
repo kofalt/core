60e0ac7fe1106c2a7565ee24dc4b2c4f73c8b308
Justin Ehlert <justinehlert@flywheel.io>
2018-07-09 15:33:58 -0500

    Add paravision archive filetypes

--- tests/unit_tests/python/test_files.py
+++ tests/unit_tests/python/test_files.py
@@ -35,3 +35,7 @@ def test_eeg():
     assert files.guess_type_from_filename('example.eeg') == 'eeg data'
     assert files.guess_type_from_filename('example.vmrk') == 'eeg marker'
     assert files.guess_type_from_filename('example.vhdr') == 'eeg header'
+
+def test_paravision():
+    assert files.guess_type_from_filename('1.pv5.zip') == 'paravision'
+    assert files.guess_type_from_filename('1.pv6.zip') == 'paravision'
