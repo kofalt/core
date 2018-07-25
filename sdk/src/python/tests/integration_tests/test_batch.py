abbe5856aa37ddf43048130f0434dab1bcd02c91
Harsha Kethineni <harshakethineni@flywheel.io>
2018-07-25 09:29:56 -0500

    tests batch with jobs, swagger fix

--- sdk/src/python/tests/integration_tests/test_batch.py
+++ sdk/src/python/tests/integration_tests/test_batch.py
@@ -17,7 +17,7 @@ class BatchTestCases(SdkTestCase):
 
     def test_batch(self):
         fw = self.fw
-       
+
         poem = 'The falcon cannot hear the falconer;'
         fw.upload_file_to_acquisition(self.acquisition_id, flywheel.FileSpec('yeats.txt', poem))
 
@@ -28,7 +28,7 @@ class BatchTestCases(SdkTestCase):
             id=self.acquisition_id
         )]
         proposal = fw.propose_batch(flywheel.BatchProposalInput(
-            gear_id=self.gear_id, 
+            gear_id=self.gear_id,
             config={},
             tags=[tag],
             targets=targets
@@ -40,7 +40,7 @@ class BatchTestCases(SdkTestCase):
         self.assertIsNotNone(proposal.origin)
         self.assertEqual(proposal.origin.type, 'user')
         self.assertNotEmpty(proposal.origin.id)
-        
+
         self.assertEqual(len(proposal.matched), 1)
         match = proposal.matched[0]
         self.assertEqual(match.id, self.acquisition_id)
@@ -50,7 +50,7 @@ class BatchTestCases(SdkTestCase):
         self.assertEmpty(proposal.ambiguous)
         self.assertEmpty(proposal.not_matched)
         self.assertEmpty(proposal.improper_permissions)
-        
+
         self.assertTimestampBeforeNow(proposal.created)
         self.assertGreaterEqual(proposal.modified, proposal.created)
 
@@ -72,8 +72,83 @@ class BatchTestCases(SdkTestCase):
         r_batch2 = fw.get_batch(proposal.id)
         self.assertEqual(r_batch2.state, 'running')
         self.assertTimestampAfter(r_batch2.modified, r_batch.modified)
-        
+
         # Cancel
         cancelled = fw.cancel_batch(proposal.id)
         self.assertEqual(cancelled, 1)
 
+    def test_batch_with_jobs(self):
+        fw = self.fw
+
+        gear = fw.get_gear(self.gear_id)
+        self.assertIsNotNone(gear)
+
+        # Make a couple jobs
+        poem = 'Mere anarchy is loosed upon the world,'
+        fw.upload_file_to_acquisition(self.acquisition_id, flywheel.FileSpec('yeats.txt', poem))
+        inputs = {
+            'any-file': flywheel.FileReference(
+                id=self.acquisition_id,
+                type='acquisition',
+                name='yeats.txt'
+            )
+        }
+        destination = flywheel.JobDestination(
+            id=self.acquisition_id,
+            type='acquisition'
+        )
+        tag = self.rand_string()
+
+        jobs = [
+            flywheel.Job(
+                gear_id=self.gear_id,
+                destination=destination,
+                inputs=inputs,
+                tags=[tag]
+            ),
+            flywheel.Job(
+                gear_id=self.gear_id,
+                destination=destination,
+                inputs=inputs,
+                tags=[tag]
+            )
+        ]
+
+        # Propose batch jobs
+        proposal = fw.create_batch_job_from_jobs(flywheel.BatchJobsProposalInput(jobs=jobs))
+
+        self.assertIsNotNone(proposal)
+
+        self.assertNotEmpty(proposal.id)
+        # Gear Id should be none, each job already knows its gear
+        self.assertIsNone(proposal.gear_id)
+        self.assertIsNotNone(proposal.origin)
+        self.assertEqual(proposal.origin.type, 'user')
+        self.assertNotEmpty(proposal.origin.id)
+
+        self.assertTimestampBeforeNow(proposal.created)
+        self.assertGreaterEqual(proposal.modified, proposal.created)
+
+        # Get
+        r_batch = fw.get_batch(proposal.id)
+        self.assertIsNotNone(r_batch)
+        # Gear Id should be none, each job already knows its gear
+        self.assertIsNone(proposal.gear_id)
+        self.assertEqual(r_batch.state, 'pending')
+
+        # Get all
+        batches = fw.get_all_batches()
+        self.assertIn(r_batch, batches)
+
+        # Start
+        jobs = fw.start_batch(proposal.id)
+        self.assertEqual(len(jobs), 2)
+
+        # Get again
+        r_batch2 = fw.get_batch(proposal.id)
+        self.assertEqual(r_batch2.state, 'running')
+        self.assertTimestampAfter(r_batch2.modified, r_batch.modified)
+
+        # Cancel
+        cancelled = fw.cancel_batch(proposal.id)
+        self.assertEqual(cancelled, 2)
