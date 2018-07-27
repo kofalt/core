9701e539ef8a3dbf5983f09d32c14756a8d20faa
Justin Ehlert <justinehlert@flywheel.io>
2018-07-27 13:28:27 -0500

    Include response message in sdk exceptions

---
+++ sdk/src/python/tests/integration_tests/test_errors.py
@@ -0,0 +1,14 @@
+import unittest
+from sdk_test_case import SdkTestCase
+
+import flywheel
+
+class ErrorTestCases(SdkTestCase):
+    def test_error_message(self):
+        try:
+            self.fw.add_job({}) # Invalid request
+            self.fail('Expected an error creating invalid job')
+        except flywheel.ApiException as e:
+            self.assertNotEmpty(e.body)
+            self.assertEqual(e.detail, 'Job must specify gear')
+
