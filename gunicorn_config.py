85b65d7e87dda57159f644d6205cd870c554f922
Justin Ehlert <justinehlert@flywheel.io>
2018-10-17 10:51:01 -0500

    Doc workers configuration value

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,13 +7,17 @@ from prometheus_client import multiprocess
 pythonpath = '/src/core'
 
 bind = '0.0.0.0:8080'
+
+# Choose number of workers based on CPU count
+# See: http://docs.gunicorn.org/en/stable/settings.html#workers
+# See: http://docs.gunicorn.org/en/stable/configure.html#configuration-file
 workers = multiprocessing.cpu_count() * 2 + 1
+
 timeout = 600 
 worker_class = 'gevent'
 
 # Capture stdout/stderr to uwsgi.log
 capture_output = True
-
 
 # Prometheus/multiproc/gunicorn compatibility
 # https://github.com/prometheus/client_python#multiprocess-mode-gunicorn
