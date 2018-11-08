fea8dde66c933cd221ecc8266cd73876fec7809b
Harsha Kethineni <harshakethineni@flywheel.io>
2018-11-08 15:24:38 -0600

    The current error for loading json isn't intuitive

--- api/config.py
+++ api/config.py
@@ -82,7 +82,11 @@ def apply_env_variables(config):
     if 'SCITRAN_AUTH_CONFIG_FILE' in os.environ:
         file_path = os.environ['SCITRAN_AUTH_CONFIG_FILE']
         with open(file_path) as config_file:
-            environ_config = json.load(config_file)
+            try:
+                environ_config = json.load(config_file)
+            except ValueError as e:
+                log.error('Could not parse json from {}'.format(file_path))
+                raise e
         config['auth'] = environ_config.get('auth', DEFAULT_CONFIG['auth'])
 
     for outer_key, scoped_config in config.iteritems():
