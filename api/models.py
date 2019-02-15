fb231954a12e2cfc91d5d75f6a76b359af39f51c
Justin Ehlert <ehlertjd@gmail.com>
2019-02-15 10:15:04 -0600

    Add base class for core models (#1537)

---
+++ api/models.py
@@ -0,0 +1,94 @@
+"""Provides a base class for sparse models.
+
+Example Usage:
+    from .. import models
+
+    class Container(models.Base):
+        def __init__(self, label, public):
+            self._id = None
+            self.label = label
+            self.public
+"""
+import collections
+
+# NOTE: Python3 - str or bytes
+STRING_TYPES = (str, unicode)
+
+class Base(collections.MutableMapping):  # pylint: disable=no-init
+    """Base class for sparse model classes.
+
+    This model pattern supports setting and retrieving arbitrary
+    attributes via the Mapping (dict) protocol. It also
+    supports deleting arbitrary attributes.
+
+    If any attribute is missing from the underlying document,
+    None will be returned instead.
+    """
+    @classmethod
+    def from_dict(cls, dct):
+        """Construct a model instance from a dictionary.
+
+        Args:
+            dct (dict): The dictionary to use
+        """
+        result = cls.__new__(cls)
+        result.__dict__ = dct  # pylint: disable=attribute-defined-outside-init
+        return result
+
+    def to_dict(self):
+        """Converts the model to a dictionary.
+
+        The resulting model should be convertable to JSON or BSON
+
+        Returns:
+            dict: The converted model
+        """
+        return _model_to_dict(self.__dict__)
+
+    def __contains__(self, key):
+        return key in self.__dict__
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __getattr__(self, name):
+        # NOTE: This is here to allow dot-access of named attributes
+        # that have been removed or are missing from the underlying dict
+        return None
+
+def _model_to_dict(obj):
+    """Flexible model conversion function
+    Conversion is prioritized as follows:
+        - Model subclasses are converted via to_dict
+        - mappings (e.g dict or OrderedDict) are converted to dict
+        - iterables (e.g. list or set) are converted to lists
+        - Everything else is left as-is
+    """
+    if isinstance(obj, Base):
+        return obj.to_dict()
+
+    if isinstance(obj, collections.Mapping):
+        result = {}
+        for key, value in obj.iteritems():
+            result[key] = _model_to_dict(value)
+        return result
+
+    if isinstance(obj, collections.Iterable) and not isinstance(obj, STRING_TYPES):
+        result = []
+        for value in obj:
+            result.append(_model_to_dict(value))
+        return result
+
+    return obj
---
+++ tests/unit_tests/python/test_base_model.py
@@ -0,0 +1,136 @@
+import pytest
+
+from api import models
+
+def test_base_model():
+    class Container(models.Base):
+        def __init__(self, label, public=False, info=None):
+            self._id = None
+            self.label = label
+            self.public = public
+            self.info = info
+
+    container = Container('Test Label', info={'my': 'value'})
+
+    assert container.label == 'Test Label'
+    assert container.public == False
+    assert container.info == {'my': 'value'}
+    assert container.foo == None # Arbitrary attribute access results in None
+
+    container._id = 'test'
+    assert container.to_dict() == {
+        '_id': 'test',
+        'label': 'Test Label',
+        'public': False,
+        'info': {'my': 'value'}
+    }
+
+    # Can delete any field, and add any additional fields
+    del container._id
+    del container.info
+    container['foo'] = 'bar'
+
+    assert container.to_dict() == {
+        'label': 'Test Label',
+        'public': False,
+        'foo': 'bar'
+    }
+
+    # Can get a field via square-brackets
+    assert container['label'] == 'Test Label'
+    # Can get with default
+    assert container.get('label', 'blah') == 'Test Label'
+    assert container.get('label2', 'blah') == 'blah'
+
+    with pytest.raises(KeyError):
+        y = container['not_exist']
+
+    # Can get keys
+    expected_keys = {'label', 'public', 'foo'}
+    assert set(container.keys()) == expected_keys
+
+    # Test In
+    assert 'label' in container
+    assert 'foo' in container
+    assert 'bar' not in container
+
+    # Test length
+    assert len(container) == 3
+
+    # Test iteration
+    for key in container:
+        assert key in expected_keys
+
+    for key, value in container.iteritems():
+        assert key in expected_keys
+        assert container[key] == value
+
+    for key, value in container.items():
+        assert key in expected_keys
+        assert container[key] == value
+
+    # Can create from a dictionary
+    c2 = Container.from_dict({
+        'label': 'Test Label',
+        'public': False,
+        'foo': 'bar'
+    })
+    assert container._id == None
+    assert container.label == 'Test Label'
+    assert container.public == False
+    assert container.info == None
+    assert container.foo == 'bar'  # Arbitrary attribute access results in None
+
+    # Test equality
+    c3 = Container(label=u'container3')
+    assert c3.to_dict() == {
+        '_id': None,
+        'label': u'container3',
+        'public': False,
+        'info': None
+    }
+
+    assert container == c2
+    assert container != c3
+    assert c2 != c3
+
+    # Test nested conversion
+    assert container.pop('foo') == 'bar'
+    with pytest.raises(KeyError):
+        container.pop('foo')
+    assert container.pop('foo', None) == None
+
+    # Test set default
+    assert container.setdefault('children', [c2, c3]) == [c2, c3]
+
+    # Test update
+    container.update({'label': 'New Label', 'foo': 'bar'})
+
+    # Test direct assignment
+    container.test = { 'nested': c3 }
+
+    assert container.to_dict() == {
+        'label': 'New Label',
+        'public': False,
+        'foo': 'bar',
+        'children': [ c2.to_dict(), c3.to_dict() ],
+        'test': { 'nested': c3.to_dict() }
+    }
+
+    # Test inheritance
+    class Session(Container):
+        def __init__(self, label, public=False, info=None, timestamp=None):
+            super(Session, self).__init__(label, public=public, info=info)
+            self.timestamp = timestamp
+
+    s1 = Session(label='session1', timestamp='yesterday')
+    assert s1.to_dict() == {
+        '_id': None,
+        'label': 'session1',
+        'public': False,
+        'info': None,
+        'timestamp': 'yesterday'
+    }
+
+    s2 = Session.from_dict(s1.to_dict())
+    assert s1 == s2
