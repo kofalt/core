82efad1fa2842577f10c15d9c9a9b4abd08564fe
Roy Collins <roycollins@flywheel.io>
2019-02-22 15:57:51 -0600

    Storage provider init via url

--- api/storage/__init__.py
+++ api/storage/__init__.py
@@ -1,3 +1,10 @@
-def create_flywheel_fs():
-    pass
-    #return storage(url)
+"""Returns appropriate storage provider based on the url provided"""
+from .py_fs_storage import PyFsStorage
+def create_flywheel_fs(url):
+
+    if url.startswith('gc'):
+        return PyFsStorage(url)
+    if url.startswith('osfs'):
+        return PyFsStorage(url)
+
+    raise ValueError('Invalid storage url supplied')
