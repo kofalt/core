89d0de143bc19000312cc5b1fb7c621f9688c177
Roy Collins <roycollins@flywheel.io>
2019-02-25 10:31:50 -0600

    Allows filepath as url in storage init

--- api/storage/__init__.py
+++ api/storage/__init__.py
@@ -1,10 +1,14 @@
 """Returns appropriate storage provider based on the url provided"""
 from .py_fs_storage import PyFsStorage
+
 def create_flywheel_fs(url):
-
-    if url.startswith('gc'):
+    """
+    This loads the storage provider based on the url provided
+    """
+    if url.startswith('gc::/'):
         return PyFsStorage(url)
-    if url.startswith('osfs'):
+    if url.startswith('osfs://'):
         return PyFsStorage(url)
 
-    raise ValueError('Invalid storage url supplied')
+    # Assume the rest are paths which we can use with osfs even though they are not urls
+    return PyFsStorage(url)
