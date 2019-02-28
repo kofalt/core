d8ce87d6252935361417461c211146e57f8a0412
Roy Collins <roycollins@flywheel.io>
2019-02-28 16:04:54 -0600

    Fix module loading error

--- api/storage/__init__.py
+++
@@ -1,9 +0,0 @@
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
-
-#import pkg_resources
-#pkg_resources.declare_namespace('flyw.storage')
-#setup(
-#    namespace_packages = ['flyw.storage']
-#)
-
