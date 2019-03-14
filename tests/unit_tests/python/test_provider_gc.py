6ab97bca106aa700affd77e16118bdad7d94d8cf
Roy Collins <roycollins@flywheel.io>
2019-03-14 11:48:52 -0500

    Convert tests to new module namespace

--- tests/unit_tests/python/test_provider_gc.py
+++ tests/unit_tests/python/test_provider_gc.py
@@ -1,47 +1,40 @@
 import datetime
-
-import bson
 import pytest
 
+from api.site.providers.gc_compute_provider import GCComputeProvider
+
 from api.web import errors
-from api.site.models import Provider, ProviderClass
-from api.site.mappers import ProviderMapper
-from api.site import providers
-from api.site.providers import GCComputeProvider
 
 origin = { 'type': 'user', 'id': 'user@test.com' }
 config = {
     'client_id': 12345,
     'client_email': 'user@google.com',
-    'private_key': '------BEGIN PROVATE KEY-----\n your key here \n------END PRIVATE KEY-----',
+    'private_key': '------BEGIN PRIVATE KEY-----\n your key here \n------END PRIVATE KEY-----',
     'private_key_id': 12321321,
     'client_x509_cert_url': 'www.google.com/mycert',
     'project_id': 'test_project'
 }
 
 def test_instantiate(mocker):
-    class_= ProviderClass.compute
-
     # The good case
     gc = GCComputeProvider(config)
     assert gc is not None
-    with mocker.patch.object(GCComputeProvider, '_validate_permissions', return_value=True ):
+    with mocker.patch.object(GCComputeProvider, '_validate_permissions', return_value=True):
         gc.validate_config()
         gc._validate_permissions.assert_called
-    
 
     config['client_id'] = None
     with pytest.raises(errors.APIValidationException):
         gc = GCComputeProvider(config)
         gc.validate_config()
     config['client_id'] = 12345
-    
+
     config['client_email'] = None
     with pytest.raises(errors.APIValidationException):
         gc = GCComputeProvider(config)
         gc.validate_config()
     config['client_email'] = 'user@google.com'
-    
+
     config['private_key_id'] = None
     with pytest.raises(errors.APIValidationException):
         gc = GCComputeProvider(config)
@@ -53,7 +46,7 @@ def test_instantiate(mocker):
         gc = GCComputeProvider(config)
         gc.validate_config()
     config['client_x509_cert_url'] = 'www.google.com/mycert'
-    
+
     config['project_id'] = None
     with pytest.raises(errors.APIValidationException):
         gc = GCComputeProvider(config)
