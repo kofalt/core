cb63cf13c1d718762674b5a6627bd111f11e44cd
Justin Ehlert <ehlertjd@gmail.com>
2019-04-18 09:46:34 -0500

    Support FW_SSL_CERT_FILE for ca-certs in SDK [FLYW-685] (#1590)

--- sdk/codegen/src/main/resources/fw-python/util.py
+++ sdk/codegen/src/main/resources/fw-python/util.py
@@ -1,5 +1,7 @@
 """Provides utility functions"""
 import collections
+import os
+
 
 def seconds_to_years(seconds):
     """Convert seconds to years"""
@@ -63,3 +65,8 @@ def to_ref(obj):
     if ref_fn:
         return ref_fn()
     return obj
+
+def set_verify_ssl(session):
+    """Create a session that verifies against correct certs"""
+    if 'FW_SSL_CERT_FILE' in os.environ:
+        session.verify = os.environ['FW_SSL_CERT_FILE']
