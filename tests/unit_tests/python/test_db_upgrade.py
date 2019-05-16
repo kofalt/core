9a6278257f8e75708aa21b7be82b4e41bf565836
Roy Collins <roycollins@flywheel.io>
2019-05-16 17:08:26 -0500

    fix merge error

--- tests/unit_tests/python/test_db_upgrade.py
+++ tests/unit_tests/python/test_db_upgrade.py
@@ -60,6 +60,9 @@ def test_all_upgrade_scripts_ran(database_mock_setup, api_db):
         for fix_id in available_fixes:
             assert getattr(fixes, fix_id).called
 
+    for check_id in checks.AVAILABLE_CHECKS:
+        assert getattr(checks, check_id).called
+
     # Upgrade Cleanup 
     # After the upgrade scripts run we will have extra providers since those
     # Are needed for all the previous tests. We should either make this test run first
@@ -68,9 +71,6 @@ def test_all_upgrade_scripts_ran(database_mock_setup, api_db):
     api_db.providers.delete_one({'label':'Static Compute'})
 
 @patch('database.get_db_version', Mock(return_value=(CDV-4, {}, {})))
-    for check_id in checks.AVAILABLE_CHECKS:
-        assert getattr(checks, check_id).called
-
 def test_necessary_upgrade_scripts_ran(database_mock_setup):
     with pytest.raises(SystemExit):
         database.upgrade_schema()
