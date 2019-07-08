fafa49a6bf9b683a69b417fa34b46770a9e21626
Roy Collins <roycollins@flywheel.io>
2019-07-08 15:12:40 -0500

    in progress

--- api/handlers/bulkhandler.py
+++ api/handlers/bulkhandler.py
@@ -4,7 +4,7 @@ import copy
 import datetime
 import dateutil
 
-from ..api import config
+from .. import config
 
 from ..auth import containerauth, always_ok
 from ..dao import containerstorage, containerutil
@@ -39,34 +39,54 @@ class BulkHandler(base.RequestHandler):
         #if not self.user_is_admin and not self.is_true('join_avatars'):
         #    self._filter_permissions(result, self.uid)
 
-        print ('we are hitting it', operation, source_cont_name, self.payload)
-
         self.dest_storage = getattr(containerstorage, containerutil.singularize(self.payload['destination_container_type']).capitalize() + 'Storage')()
         self.source_storage = getattr(containerstorage, containerutil.singularize(source_cont_name).capitalize() + 'Storage')()
-        self._validate_inputs(source_cont_name) 
+
+        source_list = []
+        dest_list = []
+        if source_cont_name != 'groups':
+            for _s in self.payload['sources']:
+                print _s
+                import sys
+                sys.stdout.flush()
+                source_list.append(bson.ObjectId(_s['_id']))
+            for _d in self.payload['destinations']:
+                dest_list.append(bson.ObjectId(_d['_id']))
+        else:
+            source_list = self.payload['sources']
+            dest_list = self.payload(['destinations'])
+
+        self._validate_inputs(source_cont_name, source_list, dest_list)
 
         getattr(self, '_' + operation + '_' + source_cont_name)()
 
-    def _validate_inputs(self, source_cont):
+    def _validate_inputs(self, source_cont, source_list, dest_list):
         """
         Validate inputs are given and exist in the system
         """
 
-        if not len(self.payload['sources']):
+        if not len(source_list):
             raise APIValidationException('You must provide at least one source')
 
-        if not len(self.payload['destinations']):
+        if not len(dest_list):
             raise APIValidationException('You must provide at least one destination')
 
-        if len(self.payload['sources']) > 1 and len(self.payload['destinations']) > 1:
+        if len(source_list) > 1 and len(dest_list) > 1:
             raise APIValidationException('You can not specify multiple sources and destinations.')
 
-        sources = self.source_storage.get_all_el(query={'_id': {'$in': self.payload['sources']}}, user=None, projection={'_id': 1})
+        sources = self.source_storage.get_all_el(query={'_id': {'$in': source_list}},
+                user=None, projection={'_id': 1})
+
+        print len(sources)
+        print sources
+        import sys
+        sys.stdout.flush()
+
         if len(sources) != len(self.payload['sources']):
             missing = set(self.payload['sources']) - set(sources)
             raise APIValidationException('The following sources are not valid: {}'.format(', '.join(str(s) for s in missing)))
 
-        dests = self.dest_storage.get_all_el(query={'_id': {'$in': self.payload['sources']}}, user=None, projection={'_id': 1})
+        dests = self.dest_storage.get_all_el(query={'_id': {'$in': self.payload['destinations']}}, user=None, projection={'_id': 1})
         if len(sources) != len(self.payload['destinations']):
             missing = set(self.payload['destinations']) - set(dests)
             raise APIValidationException('The following destinations are not valid: {}'.format(', '.join(str(s) for s in missing)))
@@ -75,10 +95,38 @@ class BulkHandler(base.RequestHandler):
     def _move_sessions(self):
 
         # We move sessions to new project for now. Add dest of session later
-        query = {}
-        source_subject_codes = config.db.subjects.find_many(query, user=None, projection={'_id': 1, 'code': 1})
+        query = {'project': {'$in': self.payload['sources']}}
+        source_subjects = config.db.subjects.find_many(query, user=None, projection={'_id': 1, 'code': 1})
+        source_subject_ids = set()
+        source_subject_codes = set()
+        for source in source_subjects:
+            source_subject_ids.add(source['_id'])
+            source_subject_codes.add(source['code'])
 
-        conflicts = config.db.subjects.find_many({'_id': 'test'})
+        conflicts = config.db.subjects.find_many({
+            'code': {'$in': source_subject_codes},
+            'project': {'$in': self.payload['destinations']}
+        })
+
+        final_conflicts = []
+        for conflict in conflicts:
+            final_conflicts.append({'_id': conflict['_id'], 'code': conflict['code']})
+
+        if self.payload.get('conflict_mode', True) or self.payload['conflict_mode'] is None or self.payload['conflict_mode'] == '':
+            print 'we have these conflicts'
+            print final_conflicts
+            import sys
+            sys.stdout.flush()
+            return final_conflicts
+
+
+
+
+
+
+
+
+
 
         # first we find conflicts as those are needed for dry run regardless.
         # Then we return that on dry run.
