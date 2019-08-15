63b2cf0248f880464fbaad38d4647a0b3a2eacb2
Ryan S. Davis <ryandavis@flywheel.io>
2019-08-15 13:57:57 -0500

    Remove as data builder now creates both providers

--- tests/integration_tests/python/test_analyses.py
+++ tests/integration_tests/python/test_analyses.py
@@ -9,15 +9,14 @@ def test_online_analysis(data_builder, as_admin, as_drone, file_form, api_db, wi
 
     group = data_builder.create_group()
     project = data_builder.create_project()
-    # Projects must have a provider for job/gear uploads to work
-    update = {'providers': {'storage': 'deadbeefdeadbeefdeadbeef'}}
 
-    # Update our specifc gear
-    api_db.gears.update({'_id': bson.ObjectId(site_gear)}, {'$set': {'gear.inputs': {'csv': {'base': 'file'}}}})
+    # Update our specific gear
+    api_db.gears.update(
+        {'_id': bson.ObjectId(site_gear)},
+        {'$set': {'gear.inputs': {'csv': {'base': 'file'}}}}
+    )
     gear = site_gear
 
-    r = as_admin.put('/projects/' + project, json=update)
-    assert r.ok
     session = data_builder.create_session()
     acquisition = data_builder.create_acquisition()
     assert as_admin.post('/acquisitions/' + acquisition + '/files', files=file_form('input.csv')).ok
